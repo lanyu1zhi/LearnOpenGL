70f7e9e1b45621793f68f891a682bc6e2e9b4727
lanyu1zhi <790872105@qq.com>
2021-07-20 16:41:48 +0800

    clear Build

--- build/extern/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/VSCode/LearnOpenGL/extern/glm
-# Build directory: G:/VSCode/LearnOpenGL/build/extern/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
